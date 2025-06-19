283a7003521c02187f0b25368d8b6ab482bf5851
Nicholas Doherty <nicholasdoherty95@gmail.com>
2025-06-18 20:23:11 -0400

    Add global bash color constants

---
+++ project/bash_colors.py
@@ -0,0 +1,15 @@
+BLACK: str      = "\033[30m"
+RED: str        = "\033[31m"
+GREEN: str      = "\033[32m"
+YELLOW: str     = "\033[33m"
+BLUE: str       = "\033[34m"
+MAGENTA: str    = "\033[35m"
+CYAN: str       = "\033[36m"
+WHITE: str      = "\033[37m"
+BRIGHT_RED: str = "\033[91m"
+
+BG_YELLOW: str  = "\033[43m"
+BG_BLUE: str    = "\033[44m"
+BG_RED: str     = "\x1b[41m"
+
+RESET = "\033[0m"
